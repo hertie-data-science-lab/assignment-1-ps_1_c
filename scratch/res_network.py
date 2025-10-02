e0b8518b3e34c199ca27fe94f309dc3d8e13fbf4
dmnkallen <dmnkallen@yahoo.com>
2025-10-02 16:51:40 +0200

    Update res_network.py

--- scratch/res_network.py
+++ scratch/res_network.py
@@ -1,36 +1,41 @@
-import time
 import numpy as np
 from scratch.network import Network
 
 class ResNetwork(Network):
-
-
     def __init__(self, sizes, epochs=50, learning_rate=0.01, random_state=1):
-        '''
-        TODO: Initialize the class inheriting from scratch.network.Network.
-        The method should check whether the residual network is properly initialized.
-        '''
-        pass
-        
-        
-
+        super().__init__(sizes, epochs=epochs, learning_rate=learning_rate, random_state=random_state)
 
     def _forward_pass(self, x_train):
-        '''
-        TODO: Implement the forward propagation algorithm.
-        The method should return the output of the network.
-        '''
-        pass
+        self.x_input = x_train
 
+    # Hidden layer 1
+        self.a1 = np.dot(self.params['W1'], x_train)
+        self.h1 = self.activation_func(self.a1)
 
+    # Hidden layer 2 (residual connection adds h1 back in)
+        self.a2 = np.dot(self.params['W2'], self.h1)
+        self.h2 = self.activation_func(self.a2) + self.h1
+
+    # Output layer
+        self.a3 = np.dot(self.params['W3'], self.h2)
+        self.output = self.output_func(self.a3)
+
+        return self.output
 
     def _backward_pass(self, y_train, output):
-        '''
-        TODO: Implement the backpropagation algorithm responsible for updating the weights of the neural network.
-        The method should return a dictionary of the weight gradients which are used to update the weights in self._update_weights().
-        The method should also account for the residual connection in the hidden layer.
+        # Error at output
+        error = self.cost_func_deriv(y_train, output)
 
-        '''
-        pass
+        # Gradient for W3
+        delta3 = error * self.output_func_deriv(self.a3)
+        grad_W3 = np.outer(delta3, self.h2)
 
+        # Gradient for W2
+        delta2 = np.dot(self.params['W3'].T, delta3) * self.activation_func_deriv(self.a2)
+        grad_W2 = np.outer(delta2, self.h1)
 
+        # Gradient for W1 (with residual connection)
+        delta1 = np.dot(self.params['W2'].T, delta2) * self.activation_func_deriv(self.a1)
+        grad_W1 = np.outer(delta1, self.x_input)  # residual connection derivative = 1
+
+        return {'W1': grad_W1, 'W2': grad_W2, 'W3': grad_W3}
